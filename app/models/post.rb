04e844dc1687fd4a2d73665634ecc6638358323a
sayanta28 <sayanta28@baiust.edu.bd>
2021-05-21 01:47:11 +0600

    Added: Custom Attribute

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    def as_json(*)
+        super.merge(
+            full_story: full_story
+        )
+    end
+
+    def full_story
+        [post_title, post_owner, post_body].join(' | ')
+    end
 end
